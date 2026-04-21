8b8577101cc8c678496ba28af6409884b58e5e3d
Hemanth3303 <109304485+Hemanth3303@users.noreply.github.com>
2026-04-21 12:49:41 +0530

    updated to use new platform and arch defines

--- Application/src/main.cpp
+++ Application/src/main.cpp
@@ -11,14 +11,48 @@ int main(int argc, char** argv) {
 	cppversion();
 
 #if defined(APPLICATION_DEBUG_BUILD)
-	std::cout << "Debug Build\n";
+	std::cout << "APPLICATION: Debug Build\n";
 #elif defined(APPLICATION_PROFILE_BUILD)
-	std::cout << "Profile Build\n";
+	std::cout << "APPLICATION: Profile Build\n";
 #elif defined(APPLICATION_RELEASE_BUILD)
-	std::cout << "Release Build\n";
+	std::cout << "APPLICATION: Release Build\n";
 #else
-
+	std::cout << "APPLICATION: Unknown Build\n";
 #endif
+
+	std::cout << "PLATFORM: ";
+#if defined(APPLICATION_PLATFORM_WINDOWS)
+	std::cout << "Windows";
+#elif defined(APPLICATION_PLATFORM_LINUX)
+	std::cout << "Linux";
+#elif defined(APPLICATION_PLATFORM_DARWIN)
+	std::cout << "Darwin";
+#elif defined(APPLICATION_PLATFORM_UNKNOWN)
+	std::cout << "Unknown";
+#else
+	std::cout << "Unknown";
+#endif
+	std::cout << "\n";
+
+	std::cout << "ARCHITECTURE: ";
+#if defined(APPLICATION_ARCHITECTURE_X86_64)
+	std::cout << "x86-64";
+#elif defined(APPLICATION_ARCHITECTURE_X86_32)
+	std::cout << "x86-32";
+#elif defined(APPLICATION_ARCHITECTURE_AARCH64)
+	std::cout << "aarch64";
+#elif defined(APPLICATION_ARCHITECTURE_AARCH32)
+	std::cout << "aarch32";
+#elif defined(APPLICATION_ARCHITECTURE_RISCV64)
+	std::cout << "riscv64";
+#elif defined(APPLICATION_ARCHITECTURE_RISCV32)
+	std::cout << "riscv32";
+#elif defined(APPLICATION_ARCHITECTURE_UNKNOWN)
+	std::cout << "unknown";
+#else
+	std::cout << "unknown";
+#endif
+	std::cout << "\n";
 
 	std::cin.get();
 
